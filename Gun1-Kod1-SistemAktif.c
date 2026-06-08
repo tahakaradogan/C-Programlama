b824125a16f576cb7dd32d6f05b8723b241e87fa
tahakaradogan <karadogantaha6@gmail.com>
2026-06-08 11:19:02 +0300

    Create Gun1-Kod1-SistemAktif.c to display message

---
+++ Gun1-Kod1-SistemAktif.c
@@ -0,0 +1,12 @@
+// GÜN 1 KOD 1
+// SENARYO: Bir endustriyel sistemde kart açıldığında ekrana "Sistem Aktif" yazısı çıkıyor
+// ALGORİTMA:
+// 1. Program başlasın
+// 2. Ekrana "Sistem Aktif" yaz
+// 3. Program bitsin
+
+#include <stdio.h> 
+int main() {
+  printf("Sistem Aktif\n")
+return 0 ;
+}
