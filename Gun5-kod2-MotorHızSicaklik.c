49495ac9a1fe61b183b75747edbc86a5c8650481
tahakaradogan <karadogantaha6@gmail.com>
2026-06-12 16:57:10 +0300

    Add initial implementation for motor speed and temperature monitoring

---
+++ Gun5-kod2-MotorHızSicaklik.c
@@ -0,0 +1,19 @@
+// GÜN 5 KOD 2
+// SENARYO: Aynı endüstriyel sistem. bu sefer sadece motor hızı değil , motorun sıcaklığı da izleniyor. 
+//          Motor çalışıyorsa ve sıcaklık normalse sistem iyi.
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
