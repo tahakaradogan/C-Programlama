6317bbee75c55d4722b9543fb03c5d953eb78e88
tahakaradogan <karadogantaha6@gmail.com>
2026-06-30 14:34:26 +0300

    Add initial comments for radar system code

---
+++ Gun8-Kod1-RadarKartSicaklik.c
@@ -0,0 +1,14 @@
+// GÜN 8 KOD 1
+// SENARYO: Bir hava savunma sisteminde 5 radar istasyonu var. Her istasyonun içinde elektronik işlem kartı bulunuyor.
+//          Bu kartlar sürekli çalıştığı için ısınıyor. 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
